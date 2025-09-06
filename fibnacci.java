78e5293aa659c67c6cf25413e9808aafdf97496b
Achyauta <achyautanandc@gmail.com>
2025-09-06 12:15:45 +0530

    Add files via upload

---
+++ fibnacci.java
@@ -0,0 +1,15 @@
+public class fibnacci {
+
+static int  fibo(int n){
+if(n<2){
+    return n;
+}
+return fibo(n-1)+fibo(n-2);
+}
+
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println(fibo(7));
+    }
+}
